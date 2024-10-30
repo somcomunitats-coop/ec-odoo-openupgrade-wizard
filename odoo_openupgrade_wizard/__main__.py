0df53b4893ed3dd2301ce263e19b479d5657686f
Álvaro García <alvarogruiz8@gmail.com>
2024-10-30 10:27:07 +0100

    [IMP] Initial commit

--- odoo_openupgrade_wizard/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Executed when module is run as a script."""
-
-from odoo_openupgrade_wizard import cli
-
-cli.main()  # pylint: disable=no-value-for-parameter
--- tests/data/extra_script/pre-migration-custom_test.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE city (
-    id int,
-    name varchar
-);
-
-insert INTO city (id, name) values (1, 'Chicago');
-insert INTO city (id, name) values (2, 'Cavalaire Sur Mer');
