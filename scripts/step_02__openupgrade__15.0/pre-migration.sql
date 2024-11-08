44ceb27c784140d09b9ee6ed3b01ab01a27c8d41
Álvaro García <alvarogruiz8@gmail.com>
2024-11-08 11:39:01 +0100

    [IMP] Clean date of companies

--- scripts/step_02__openupgrade__15.0/pre-migration.sql
+++ scripts/step_02__openupgrade__15.0/pre-migration.sql
@@ -1,1 +1,10 @@
-
+DELETE FROM ir_model_data where id in (SELECT
+    ir_model_data.id
+FROM
+    ir_model_data
+LEFT JOIN
+    res_company
+ON
+    res_company.id = CAST(SPLIT_PART(ir_model_data.name, '_', 1) AS INTEGER)
+WHERE
+    ir_model_data.name ~ '^[0-9]+_' and res_company.id is null);
