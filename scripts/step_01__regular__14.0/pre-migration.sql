5b5e85c69d7a268ff990d8f03b6763171504e49c
daniquilez <dani.quilez@gmail.com>
2024-12-05 15:43:24 +0100

    script of db executed pre migration

--- scripts/step_01__regular__14.0/pre-migration.sql
+++ scripts/step_01__regular__14.0/pre-migration.sql
@@ -1,3 +1,3 @@
-update ir_cron set active = False;
-update ir_mail_server set active = False;
-update fetchmail_server set active = False;
+-- update ir_cron set active = False;
+-- update ir_mail_server set active = False;
+-- update fetchmail_server set active = False;
